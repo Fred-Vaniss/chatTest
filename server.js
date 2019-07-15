ebbad4a3f0ee73bebec0034c23a60de308cb8cc4
Frédérick Van Isschot <46893914+Fred-Vaniss@users.noreply.github.com>
2019-07-15 12:12:34 +0200

    Changements port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 app.use(express.static(__dirname + "/"));
 
-let port = 80
+let port = process.env.PORT || 4050
 let server = app.listen(port, () => console.log(`Server started at port ${port}`));
 let client =  require("socket.io")(server);
 
